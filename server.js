c94c4546eea9de0fe1bd1e32fa5023f84fae263f
Jared Gutkin <jgutkinsam@gmail.com>
2022-06-10 20:50:30 -0500

    morgan implemented

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const PORT = 3001
 app.use(express.json())
 app.use(morgan('tiny'))
 
+morgan.token('body', function(req, res){
+  return `${JSON.stringify(req.body)}`
+})
+
+app.use(morgan(':method :url :status :res[content-length]- :response-time ms :body'))
+
 
 let persons = [
     { 
